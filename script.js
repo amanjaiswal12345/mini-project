05e1a61bcfe4639c361bf0f3e160e656160ca07a
amanjaiswal12345 <aj269035@gmail.com>
2023-06-15 14:19:15 +0530

    project

---
+++ script.js
@@ -0,0 +1,37 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f2697e0942msh7037b6e67c888f8p1c87c5jsne8029cf96b54',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+const getWeather = (city) => {
+	cityName.innerHTML= city
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +city , options)
+	.then(response => response.json())
+	.then((response) => {
+		console.log(response)
+		                     
+		temp.innerHTML  =  response.temp
+		temp2.innerHTML  =  response.temp
+		feels_like.innerHTML  = response.feels_like
+		humidity.innerHTML  = response.humidity
+		humidity2.innerHTML  = response.humidity
+		min_temp.innerHTML  = response.min_temp
+		max_temp.innerHTML  = response.max_temp
+		wind_speed.innerHTML  = response.wind_speed
+		wind_speed2.innerHTML  = response.wind_speed
+		wind_degrees.innerHTML  = response.wind_degrees
+		Sunrise.innerHTML  = response.sunrise
+		Sunset.innerHTML  = response.sunset
+	})
+
+	.catch(err => console.error(err));
+}
+submit.addEventListener("click" ,(e)=>{
+	  e.preventDefault()
+      getWeather(city.value)
+})
+getWeather("Delhi")
+
+
